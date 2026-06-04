f23f185c3545141c03dcbf0434c6c90126f34f1a
Shahid Raza <raza59261@gmail.com>
2026-06-04 05:48:28 +0530

    solved

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let sounds = {
+  applause: new Audio("./sounds/applause.mp3"),
+  boo: new Audio("./sounds/gasp.mp3"),
+  gasp: new Audio("./sounds/gasp.mp3"),
+  tada: new Audio("./sounds/tada.mp3"),
+  victory: new Audio("./sounds/victory.mp3"),
+  wrong: new Audio("./sounds/wrong.mp3"),
+};
+const stopBtn = document.getElementById("stop");
+function stopAllSound() {
+  Object.values(sounds).forEach((sound) => {
+    sound.pause();
+    sound.currentTime = 0;
+  });
+}
+
+Object.keys(sounds).forEach((key) => {
+  const btn = document.getElementById(key);
+  if (btn) {
+    btn.addEventListener("click", () => {
+      stopAllSound();
+      sounds[key].play();
+    });
+  }
+});
+stopBtn.addEventListener("click", stopAllSound);
